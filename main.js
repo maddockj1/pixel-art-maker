0b4a61cf7b96e7898159bbe189625947359d1e3e
maddockj1 <jmmaddock@gmail.com>
2018-09-28 15:39:16 -0600

    added DOM generated grid

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-box = document.getElementsByClassName("pixel");
+
+  box = document.getElementsByClassName("pixel");
 document.addEventListener("DOMContentLoaded", function(event) {
   //console.log("DOM fully loaded and parsed");
+  function createRowDivs() {
+    for (let i = 0; i <20; i++) {
+      let row = document.createElement('div')
+      row.classList.add('row')
 
+    for (let j = 0; j < 20; j++) {
+      art = document.createElement('div')
+      art.classList.add('pixel')
+      row.appendChild(art)
+    }
+    document.getElementById('table').appendChild(row)
+   }
+  }
+  createRowDivs()
   //create a variable to use for all buttons
-
+// let COLORS = ['red', 'green', 'blue', 'yellow', 'black', 'orange']
+// function createColors(){
+//   for(let color of COLORS) {
+//     console.log('adding', color)
+//     let div = document.createElement('div')
+//     div.setAttribute('data-color', color)
+//     div.classList.add('pallete-color')
+//     div.classList.add(color)
+//     div.classList.add("coulors")
+//     div.style.backgroundColor=color
+//     document.getElementById("table").appendChild(div)
+// }
+// }
+// createColors()
 let color = document.getElementById("colorPicker");
 //document.color.addEventListener("click", function(){
 
@@ -14,5 +41,8 @@ for(let i=0;i<box.length;i++){
   })
 }
   //console.log("changeColor is running!");
+
+
+//console.log("changeColor is running!");
 
 });
